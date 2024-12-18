a33f119b1fdd9b7bc6a9aa6ca1c41fc0161ec320
nothing <2727219897@qq.com>
2024-12-18 20:37:20 +0800

    4又写了一点，敲了半天JsClear，讲道理有点累

--- sylar/log.cc
+++ sylar/log.cc
@@ -115,7 +115,7 @@ void LogFormatter::init()
     // str, format, type
     std::vector<std::tuple<std::string, std::string, int>> vec;
 
-    std::string str;
+    std::string nstr;
     for (size_t i = 0; i < m_pattern.size(); ++i)
     {
         if (m_pattern[i] != '%')
@@ -125,20 +125,52 @@ void LogFormatter::init()
         }
         size_t n = i + 1;
         int fmt_status = 0;
+        size_t fmt_begin = 0;
 
         std::string str;
         std::string fmt;
+
         while (n < m_pattern.size())
         {
             if (isspace(m_pattern[n])) break;
             if (fmt_status == 0)
             {
-                if (m_pattern[n] == '(')
+                if (m_pattern[n] == '{')
                 {
-
+                    str = m_pattern.substr(i + 1, n - i);
+                    fmt_status = 1; // 解析格式
+                    fmt_begin = n;
+                    ++n;
+                    continue;
+                }
+            }
+            if (fmt_status == 1)
+            {
+                if (m_pattern[n] == '}')
+                {
+                    fmt = m_pattern.substr(fmt_begin + 1, n - fmt_begin - 1);
+                    fmt_status = 2;
+                    break;
                 }
             }
             
+            if (fmt_status == 0)
+            {
+                vec.push_back(std::make_pair(nstr, "", 0));
+                str = m_pattern.substr(i + 1, n - i - 1);
+                vec.push_back(std::make_tuple(str, fmt, 1));
+                i = n;
+            }
+            else if (fmt_status == i)
+            {
+                std::cout << "pattern parse error: " << m_pattern << " - " << m_pattern << std::endl; 
+                vec.push_back(std::make_tuple("<pattern_error>", fmt, 0));
+            }
+            else if (fmt_status == 2)
+            {
+                vec.push_back(std::make_tuple(str, fmt, 1));
+                i = n;
+            }
         }
     }
 }
